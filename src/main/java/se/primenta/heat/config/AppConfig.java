dc00dc749a6ec57bcf3c82322383c639363bf6dc
Sven Wesley <sven.wesley@gmail.com>
2014-06-28 17:44:41 +0200

    First initial (empty) project

---
+++ src/main/java/se/primenta/heat/config/AppConfig.java
@@ -0,0 +1,9 @@
+package se.primenta.heat.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "se.primenta.heat")
+public class AppConfig {
+}
